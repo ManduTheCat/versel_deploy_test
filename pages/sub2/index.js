3c4b15212a4c4e65c742d46778daf4ab50328c15
ManduTheCat <krocd923@gmil.com>
2022-06-28 11:42:54 +0900

    route anything

---
+++ pages/sub2/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+export default function Index() {
+  return (
+    <>
+      <ol>
+        <li>
+          <Link href="/sub2/about">/sub2/about</Link>
+        </li>
+        <li>
+          <Link href="/sub2/20">/sub2/20</Link>
+        </li>
+      </ol>
+      /sub2/index.js
+    </>
+  );
+}
